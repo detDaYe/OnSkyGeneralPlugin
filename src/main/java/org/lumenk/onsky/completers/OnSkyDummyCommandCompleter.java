44a9d4c599e93c5ba554264cfac79b31235b1569
detDaye <mindaye8504@gmail.com>
2021-09-29 23:54:29 +0900

    bug fix complete

--- src/main/java/org/lumenk/onsky/completers/OnSkyDummyCommandCompleter.java
+++ src/main/java/org/lumenk/onsky/completers/OnSkyDummyCommandCompleter.java
@@ -8,7 +8,7 @@ import org.bukkit.command.TabCompleter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class OnSkyDummyCompleter implements TabCompleter {
+public class OnSkyDummyCommandCompleter implements TabCompleter {
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         List<String> result = new ArrayList<>();
